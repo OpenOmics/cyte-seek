1c260665041fbb6595e6c6d9c2f50d62257b8a03
skchronicles <kuhnsa3@gmail.com>
2022-07-08 17:56:05 -0400

    Adding retry decorator to try any func N times with an exponential backoff stragety

--- src/shells.py
+++ src/shells.py
@@ -4,10 +4,53 @@
 # Python standard library
 from __future__ import print_function
 from subprocess import CalledProcessError
-import os, subprocess
+import os, subprocess, time
 
 # Local imports
 from utils import fatal, err
+
+
+def retry(times=5, exceptions=(Exception)):
+    """
+    Decorator to retry running a function. Retries the wrapped function 
+    N times with an exponential backoff stragety. A tuple of Exceptions
+    can be passed that trigger a retry attempt. When times is equal to 
+    4 the back-off strategy will be {4, 16, 64, 256} seconds. Calls fatal
+    if the function cannot be run within the defined number of times.
+    @param times <int>: 
+        The number of times to repeat the wrapped function,
+        default: 5
+    @param exceptions tuple(<Exception>):
+        Tuple of Python Exceptions that will trigger a retry attempt,
+        default: (Exception)
+    @return <object func>:
+        Calls fatal when func cannot be run after N times.
+    """
+    def decorator(func):
+        def do(*args, **kwargs):
+            # Begin the attempt loop and return if successful 
+            attempt = 0
+            delay = 1
+            while attempt < times:
+                try:
+                    return func(*args, **kwargs)
+                except exceptions as e:
+                    # Default expection, Exception
+                    err('Function failed: {0}'.format(func.__name__))
+                    err('\t@args: {0}'.format(args))
+                    err('\t@kwargs: {0}'.format(kwargs))
+                    err('\t@reason: {0}'.format(e))
+                    # Increase backoff: 4, 16, 64, 256, 1024, 4096...
+                    attempt += 1
+                    delay = 4**attempt
+                    err("Attempt: {0}/{1}".format(attempt, times))
+                    err("Trying again in {0} seconds!\n".format(delay))
+                    time.sleep(delay)
+            # Could not succesfully run the given function 
+            # in its alloted number of tries, exit with error 
+            fatal('Fatal: failed after max retry attempts!')
+        return do
+    return decorator
 
 
 def set_options(strict):
@@ -31,6 +74,7 @@ def set_options(strict):
     return prefix
 
 
+@retry(times=5, exceptions=(CalledProcessError))
 def bash(cmd, interpreter='/bin/bash', strict=set_options(True), cwd=os.getcwd(), **kwargs):
     """
     Interface to run a process or bash command. Using subprocess.call_check()
@@ -48,19 +92,13 @@ def bash(cmd, interpreter='/bin/bash', strict=set_options(True), cwd=os.getcwd()
     @return exitcode <int>:
         Returns the exit code of the run command, failures return non-zero exit codes
     """
-    try:
-        exitcode = subprocess.check_call(strict + cmd, 
-            shell=True, 
-            executable=interpreter, 
-            cwd=cwd, 
-            **kwargs
-        )
-    except CalledProcessError as e:
-        exitcode = e.returncode
-        err("""WARNING: Failed to run '{}' command!
-        └── Command returned a non-zero exitcode of '{}'.""".format(strict + cmd, exitcode)
-        )
-
+    exitcode = subprocess.check_call(strict + cmd, 
+        shell=True, 
+        executable=interpreter, 
+        cwd=cwd, 
+        **kwargs
+    )
+    
     return exitcode
 
 
@@ -68,3 +106,4 @@ if __name__ == '__main__':
     # Tests
     bash('ls -la /home/')
     bash('ls -la /fake/dne/path')
+
